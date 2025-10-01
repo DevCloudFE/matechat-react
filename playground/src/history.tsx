fba56ab07a869cb194df83c0eafacdcf85e53dbf
星弦语 <xu060805@gmail.com>
2025-10-02 03:04:54 +0800

    fix: lint

--- playground/src/history.tsx
+++ playground/src/history.tsx
@@ -1,6 +1,6 @@
 import { useTranslation } from "react-i18next";
-import historyImg from './assets/history.png'
-import searchIcon from './assets/search.svg'
+import historyImg from "./assets/history.png";
+import searchIcon from "./assets/search.svg";
 
 function History() {
   const { t } = useTranslation();
@@ -10,7 +10,10 @@ function History() {
       <span className="text-sm font-bold mb-2">{t("history")}</span>
       <div className="bg-gray-50 h-9 rounded-2xl flex flex-row items-center px-3">
         <img src={searchIcon} alt="search" />
-        <input placeholder={t('search')} className="mx-2 text-sm text-gray-600" />
+        <input
+          placeholder={t("search")}
+          className="mx-2 text-sm text-gray-600"
+        />
       </div>
       <div className="size-full flex items-center justify-center">
         <img src={historyImg} alt="history" />
